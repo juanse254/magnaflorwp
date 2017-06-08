6316c957cf35d06f3bb92391dd2cd3c8be34aa92
Sebastian <rtl20172017@gmail.com>
2017-06-08 17:20:23 +0000

    Ray mod for disabling range of dates on product display (miniscript)

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,2 +1,30 @@
 <?php
 // Add custom Theme Functions here
+
+/**
+ * @snippet       Disable Variable Product Price Range
+ * @how-to        Watch tutorial @ https://businessbloomer.com/?p=19055
+ * @sourcecode    https://businessbloomer.com/disable-variable-product-price-range-woocommerce/
+ * @author        Rodolfo Melogli
+ * @compatible    WooCommerce 2.4.7
+ */
+ 
+add_filter( 'woocommerce_variable_sale_price_html', 'bbloomer_variation_price_format', 10, 2 );
+add_filter( 'woocommerce_variable_price_html', 'bbloomer_variation_price_format', 10, 2 );
+ 
+function bbloomer_variation_price_format( $price, $product ) {
+ 
+// Main Price
+$prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
+$price = $prices[0] !== $prices[1] ? sprintf( __( 'From: %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+ 
+// Sale Price
+$prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
+sort( $prices );
+$saleprice = $prices[0] !== $prices[1] ? sprintf( __( 'From: %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+ 
+if ( $price !== $saleprice ) {
+$price = '<del>' . $saleprice . $product->get_price_suffix() . '</del> <ins>' . $price . $product->get_price_suffix() . '</ins>';
+}
+return $price;
+}
